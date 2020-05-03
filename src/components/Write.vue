52fbf4a4ce315d2339ee6939370d59f6294bd7cf
SinggleXGC <1307708161@qq.com>
2020-05-03 21:14:17 +0800

    发布文章页面设计，未完成

---
+++ src/components/Write.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="xgc-padding-tiny">
+        <div class="xgc-content-container">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Write"
+    }
+</script>
+
+<style scoped>
+
+</style>
