5b6858550a1a9e732ffc61bbb67aa9974f870fe4
SinggleXGC <1307708161@qq.com>
2020-05-07 16:49:31 +0800

    刚创建的项目文件

---
+++ src/main/java/com/xgc/notesblog/NotesblogApplication.java
@@ -0,0 +1,13 @@
+package com.xgc.notesblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xgc/notesblog/NotesblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgc.notesblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
